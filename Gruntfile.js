2d67a656633c6a65893f62d965f5093ec7867493
nimiby <nimrod.benyaakov@verint.com>
2014-09-26 10:58:56 -0700

    Change Project type

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+﻿/// <vs AfterBuild='dummyTask' />
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: {
+            install: { }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.registerTask('build', ['bower']);
+}
